8824f3c85493d4eb72f7b71af642de92f856ad23
Mateusz <mateuszprokop@outlook.com>
2019-03-13 10:15:58 +0100

    added base structure with folders

---
+++ src/main/java/com/wsiiz/gamemanager/GamemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.wsiiz.gamemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamemanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamemanagerApplication.class, args);
+    }
+
+}
